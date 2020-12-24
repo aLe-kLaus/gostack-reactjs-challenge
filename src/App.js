4f0acbbe5b4f6b09b5a65ca07c31b690dc88645f
Alessom Klaus <alessomklaus13@gmail.com>
2020-12-24 11:01:26 -0300

    Resolved

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-import React from "react";
-
+import React, {useState, useEffect} from "react";
+import api from './services/api'
 import "./styles.css";
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect (() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
-  }
+    const response = await api.post('projects', {
+      title: `New ${Date.now()}`,
+      owner: "Alessom Klaus"
+    })
+
+    setProjects([ ...projects, response.data]);
+  };
 
   async function handleRemoveRepository(id) {
-    // TODO
-  }
+    const response =  await api.delete(`projects/${id}`);
+
+    setProjects(projects.filter(
+      project => project.id !== id
+    ))
+  };
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {projects.map(project => (
+        <li key={project.id}>
+          {project.title}
+          <button onClick={() => handleRemoveRepository(project.id)}>Remover</button>
+          </li>
+        ))}
+      <li>
+      </li>
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
